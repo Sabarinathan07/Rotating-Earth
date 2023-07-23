8c342d37209fbe07ee386dab33cae4c2350e7aba
Madhumitha155 <madgood80@gmail.com>
2023-07-23 18:13:43 +0530

    Included libraries

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-const THREE=require('three');
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 var container, controls, camera, renderer, scene, light,
     rotationSpeed = 0.1,
     clock = new THREE.Clock(),
