8173eeaec924541e725f7f7dc788a936a6b8b97c
Sabari <r.sabarinathan02@gmail.com>
2023-07-23 17:59:11 +0530

    bugs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
-
-app.listen(3000, () =>
-  console.log('Visit http://127.0.0.1:3000')
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log('Visit http://localhost:3000')
 );
